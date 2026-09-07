41c8b770887279d3932284249c8e3d53643a2a90
Juan Treminio <jtreminio@gmail.com>
2026-09-06 20:51:50 -0500

    Revert "Remove edit stuff, it is in core now"
    This reverts commit 7ec3a4445d392fdbc13bd82773f8cbbcbc3708a7.

--- src/Generated/PruneManifest.g.cs
+++ src/Generated/PruneManifest.g.cs
@@ -13,6 +13,8 @@ public static class PruneManifest
     public static readonly string[] AlwaysKeep = new string[]
     {
         "ConditioningKrea2RebalanceNode",
+        "Krea2EditModelPatchNode",
+        "Krea2OstrisEditModelPatchNode",
     };
     // PRUNE-MANIFEST-END
 }
